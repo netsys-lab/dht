90b516e868883df931f12f308fa246e43018c888
Clemens Beck <clemens.beck97@gmx.de>
2021-11-18 16:30:22 +0100

    rollback to token implementation suggested in BEP5

--- tokens.go
+++ tokens.go
@@ -30,11 +30,8 @@ func (me tokenServer) createToken(addr Addr, t time.Time) string {
 		panic(ip)
 	}
 	h.Write(ip)
-
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], uint64(addr.IA().IAInt()))
-	h.Write(b[:])
 	ti := t.UnixNano() / int64(me.interval)
+	var b [8]byte
 	binary.BigEndian.PutUint64(b[:], uint64(ti))
 	h.Write(b[:])
 	h.Write(me.secret)
